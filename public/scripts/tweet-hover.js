c1893726b4ff78bebef93c22a64a12c2e028e143
Vanessa Power <vanessa.l.power@gmail.com>
2022-10-01 13:11:54 -0700

    adding the hover effect on the tweet icons

--- public/scripts/tweet-hover.js
+++ public/scripts/tweet-hover.js
@@ -13,12 +13,23 @@ $(document).ready(function () {
   //     counter.css("color", "black");
   //   }
   // });
-  $(".tweet").on("mouseover", function (event) {
+  $(".tweet").on("mouseover", function () {
     // console.log(this);
     $(this).css("box-shadow", "10px 10px 5px #b3bbd9");
   });
-  $(".tweet").on("mouseout", function (event) {
-    console.log(this);
+  $(".tweet").on("mouseout", function () {
+    // console.log(this);
     $(this).css("box-shadow", "none");
+  });
+
+  $(".tweet-icons").on("mouseover", function (event) {
+    const target = $(event.target);
+    // console.log(target);
+    if (target.is("i")) {
+      target.css("color", "#FFAC1C");
+    }
+  });
+  $(".tweet-icons").on("mouseout", function (event) {
+    $(this).find("i").css("color", "#4056A1");
   });
 });
